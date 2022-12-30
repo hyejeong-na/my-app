dc441a66ae2dc4314bda0e7a9eea5e015f8f1e9d
nhj1987 <nhj1987@cj.net>
2022-12-30 16:23:05 +0900

    nginx delete

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        이 기기의 IP주소ddd는 {ip}입니다.
+        이 기기의 IP주소ddd는  {ip}입니다.
       </header>
     </div>
   );
